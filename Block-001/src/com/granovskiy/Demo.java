a2b37b2cfdba71548471f6028fa1f8da18ac2095
Sviatoslav <s.granovskiy@gmail.com>
2019-06-07 09:16:10 +0300

    printing is added

--- Block-001/src/com/granovskiy/Demo.java
+++ Block-001/src/com/granovskiy/Demo.java
@@ -4,5 +4,8 @@ public class Demo {
     public static void main(String[] args) {
         Cat cat1 = new Cat("Vaska", 3);  // bad practice
         Cat cat2 = Cat.of("Tom", 2);     //  good practice
+        System.out.println(cat1);
+        System.out.println(cat2);
+
     }
 }
