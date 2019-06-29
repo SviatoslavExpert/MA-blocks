98d5ee9cff234ab30d23e3aa418bc43ff0b7db57
Sviatoslav <s.granovskiy@gmail.com>
2019-06-29 11:21:29 +0300

    arrays code added

--- Block-002/src/com/granovskiy/Demo.java
+++ Block-002/src/com/granovskiy/Demo.java
@@ -1,4 +1,71 @@
 package com.granovskiy;
 
 public class Demo {
+    public static void main(String[] args) {
+        //  primitives are copied by value
+        int a = 10;
+        int b = a;
+        b++;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println();
+        //  references pointing to the same object
+        Cat cat = new Cat("vaska", 3);
+        Cat cat1 = cat; // 2 references point to the same object
+        cat1.age = 4;
+        System.out.println("cat age is:" + cat.age);
+        System.out.println("cat1 age is:" + cat1.age);
+        System.out.println();
+        //  calling the method that changes field value
+        Cat cat2;
+        cat2 = doSmth(cat);
+        System.out.println("cat age is:" + cat.age);
+        System.out.println("cat1 age is:" + cat1.age);
+        System.out.println("cat2 age is:" + cat2.age);
+        System.out.println();
+        //  calling the method makeNewCatObject
+        Cat cat3;
+        cat3 = makeNewCatObject(cat);
+        System.out.println("cat age is:" + cat.age);
+        System.out.println("cat1 age is:" + cat1.age);
+        System.out.println("cat2 age is:" + cat2.age);
+        System.out.println("cat3 age is:" + cat3.age);  //  only cat3's age changed
+
+    }
+
+    private static Cat doSmth(Cat cat) {
+        cat.age = 5; //  object's field value is changed
+        return cat;
+    }
+
+    private static Cat makeNewCatObject(Cat cat) {
+        cat = new Cat("Tom", 7);
+        return cat;
+    }
+
+    static class Cat {
+        private String name;
+        private int age;
+
+        public Cat(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
 }
---
+++ Block-002b/src/com/granovskiy/Demo.java
@@ -0,0 +1,48 @@
+package com.granovskiy;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] array = {1,3,5,7,9};
+        doSmth(array);
+    }
+
+    /* it's very important to make a copy
+        of an array before working with it!
+        You cannot change the original array.
+     */
+    private static void doSmth(int[] arr) {
+        System.out.println("These are 3 ways of copying arrays.");
+        System.out.println();
+        //  the 1-st way to copy an array: array iteration
+        System.out.print("This is copy1 (the 1-st way of copying): ");
+        int[] copy1 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            copy1[i] = arr[i];
+            System.out.print(copy1[i]);
+        }
+        System.out.println();
+        System.out.println();
+
+        //  the 2-nd way to copy an array: System.arraycopy();
+        System.out.print("This is copy2 (the 2-nd way of copying): ");
+        int[] copy2 = new int[arr.length];
+        System.arraycopy(arr, 0, copy2, 0, arr.length);
+        //  for each is a special kind of loop for working with arrays
+        for (int i : copy2) {  //  variable i takes (in turns) values of the array elements
+            System.out.print(i);
+        }
+        System.out.println();
+        System.out.print("This is another way of printing array copy2: ");
+        System.out.print(Arrays.toString(copy2));
+        System.out.println();
+        System.out.println();
+
+        //  the 3-d way to copy an array: Arrays.copyOf();
+        System.out.print("This is copy3 (the 3-d way of copying): ");
+        int[] copy3 = Arrays.copyOf(arr, arr.length);
+        System.out.print(Arrays.toString(copy2));
+        
+    }
+}
