fda4a0298e4f9404d0b0d02298ce3e029305e94e
Sviatoslav <s.granovskiy@gmail.com>
2019-07-17 17:00:03 +0300

    lambda added

--- Block-006d-InterfaceExample2/src/com/granovskiy/Demo.java
+++ Block-006d-InterfaceExample2/src/com/granovskiy/Demo.java
@@ -4,6 +4,10 @@ public class Demo{
     public static void main(String[] args) {
         Has has = new Has(new NewCollaborator());
         has.doSmth();
+
+        //  another example of lambda
+        Collaborator collaborator = () -> System.out.println("TEST");
+        collaborator.doSmth();
     }
 
 }
